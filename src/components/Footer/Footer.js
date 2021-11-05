7ef86897e2b0039197edb6b918d6285fb1fd298e
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2021-11-05 22:53:04 +0530

    💄 Footer ui updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import Image from 'next/image'
+import nexusLogo from '../../assets/branding/NexusLogoWhite1250x225.png'
+import walletLogo from '../../assets/icons/wallet.png'
+import appleLogo from '../../assets/icons/apple.png'
+import playstoreLogo from '../../assets/icons/playstore.png'
 
 function Footer() {
     return (
         <div className={styles.footer}>
-            im a footer
+            <Image width={220} height={40} src={nexusLogo}></Image>
+            <div className={styles.walletLinks}>
+            <Image  src={walletLogo}></Image>
+                <Image  src={appleLogo}></Image>
+
+            <Image src={playstoreLogo}></Image>
+            </div>
         </div>
     )
 }
