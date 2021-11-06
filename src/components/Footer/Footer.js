a0ae42dda8b19dc2936f7bffdaf696bfa2d00094
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2021-11-06 10:32:47 +0530

    💄 Added Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,51 @@
-import React from 'react'
-import styles from './Footer.module.css'
-import Image from 'next/image'
-import nexusLogo from '../../assets/branding/NexusLogoWhite1250x225.png'
-import walletLogo from '../../assets/icons/wallet.png'
-import appleLogo from '../../assets/icons/apple.png'
-import playstoreLogo from '../../assets/icons/playstore.png'
+import styles from './Footer.module.css';
+import Image from 'next/image';
+import nexusLogo from '../../assets/branding/NexusLogoWhite1250x225.png';
+import walletLogo from '../../assets/icons/wallet.png';
+import appleLogo from '../../assets/icons/apple.png';
+import playstoreLogo from '../../assets/icons/playstore.png';
 
 function Footer() {
-    return (
-        <div className={styles.footer}>
-            <Image width={220} height={40} src={nexusLogo}></Image>
-            <div className={styles.walletLinks}>
-            <Image  src={walletLogo}></Image>
-                <Image  src={appleLogo}></Image>
+  return (
+    <div className={styles.footer}>
+      <section>
+        <Image width={220} height={40} src={nexusLogo} alt="nexus logo"></Image>
+        <div className={styles.walletLinks}>
+          <div>
+            <Image
+              width="48"
+              height="48"
+              src={walletLogo}
+              alt="Wallet Logo"></Image>
+            <div>Desktop Wallet</div>
+          </div>
 
-            <Image src={playstoreLogo}></Image>
-            </div>
+          <div>
+            <Image
+              width="48"
+              height="48"
+              src={appleLogo}
+              alt="Apple Logo"></Image>
+            <div>Download from IOS store</div>
+          </div>
+
+          <div>
+            <Image
+              width="48"
+              height="48"
+              src={playstoreLogo}
+              alt="playstore logo"></Image>
+            <div>Get it on Playstore</div>
+          </div>
         </div>
-    )
+      </section>
+      <section>
+        <p>
+          Support Contact | <span>contact@nexus.io</span>
+        </p>
+      </section>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,26 @@
-import styles from "./Navbar.module.css";
-import Image from "next/image";
-import nexusLogo from "../../assets/icons/NexuslogoBlue1000.png";
-import Link from 'next/link'
+import styles from './Navbar.module.css';
+import Image from 'next/image';
+import nexusLogo from '../../assets/icons/NexuslogoBlue1000.png';
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <div className={styles.nav}>
       <div className={styles.brand}>
-        <Image width={145} height={32} layout="fixed" src={nexusLogo}></Image>
+        <Image
+          width={145}
+          height={32}
+          layout="fixed"
+          alt="nexus logo"
+          src={nexusLogo}></Image>
         <div className={styles.explorer}>Explorer</div>
       </div>
       <div className={styles.links}>
-          <Link href="/">Home</Link>
-          <Link href="/trustlist">Trustlist</Link>
-          <Link href="/richlist">Richlist</Link>
-          <Link href="/network">Network</Link>
-          <Link href="/about">About</Link>
+        <Link href="/">Home</Link>
+        <Link href="/trustlist">Trustlist</Link>
+        <Link href="/richlist">Richlist</Link>
+        <Link href="/network">Network</Link>
+        <Link href="/about">About</Link>
       </div>
     </div>
   );
