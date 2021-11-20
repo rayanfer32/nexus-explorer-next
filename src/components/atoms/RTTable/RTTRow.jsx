42a6cfee63fde369e6fa00834d6d918765a0361f
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-11-20 23:50:53 +0530

    feat: 🎨 improve the txn table row

--- src/components/atoms/RTTable/RTTRow.jsx
+++ src/components/atoms/RTTable/RTTRow.jsx
@@ -2,44 +2,64 @@ import styles from './RTTRow.module.css';
 
 function RTTRow(props) {
   const {
-    fromId,
-    toId,
-    txnId,
-    operation,
-    txType,
-    amount,
-    confirmations,
-    contracts,
+    fromId = '',
+    toId = '',
+    txnId = '',
+    operation = '',
+    txType = '',
+    amount = '',
+    amountUnit = '',
+    confirmations = '',
+    contracts = '',
   } = props;
 
   return (
     <div className={styles.rttrow}>
-      <div className={styles.rttrowTop}>
-        <div className={styles.fromTag}>
-          <div className={styles.tagText}>From:{fromId}</div>
+      <div className={styles.rttrowName}>{operation}</div>
+      <div className={styles.rttrowDetail}>
+        {/* row top */}
+        <div className={styles.rttrowTop}>
+          <div className={styles.fromTag}>
+            From: <span className={styles.tagText}>{fromId}</span>
+          </div>
+          <div className={styles.toTag}>
+            To: <span className={styles.tagText}> {toId}</span>
+          </div>
         </div>
-        <div className={styles.toTag}>
-          <div className={styles.tagText}>To: {toId}</div>
-        </div>
-      </div>
-      <div className={styles.rttrowBottom}>
-        <div className={styles.rttrowName}>{operation}</div>
-        <div className={styles.txnType}>
-          {txType}
-          <div className={styles.lowerThirdText}>type</div>
-        </div>
-        <div className={styles.txnId}>
-          <div className={styles.txnIdValue}>{txnId}</div>
-          <div className={styles.lowerThirdText}>txn_id</div>
-        </div>
+        {/* row bottom */}
+        <div className={styles.rttrowBottom}>
+          <div className={styles.txnType}>
+            {!!txType && (
+              <>
+                <div className={styles.txnTypeText}>{txType}</div>
+                <div className={styles.lowerThirdText}>type</div>
+              </>
+            )}
+          </div>
+          <div className={styles.txnId}>
+            {!!txnId && (
+              <>
+                <div className={styles.txnIdValue}>{txnId}</div>
+                <div className={styles.lowerThirdText}>txn_id</div>
+              </>
+            )}
+          </div>
 
-        <div className={styles.txnAmount}>
-          {amount} NXS<div className={styles.lowerThirdText}>amount</div>
+          <div className={styles.txnAmount}>
+            {!!amount && (
+              <>
+                <div className={styles.txnAmountText}>
+                  {amount} {amountUnit}
+                </div>
+                <div className={styles.lowerThirdText}>amount</div>
+              </>
+            )}
+          </div>
+          <div className={styles.confirmations}>
+            Confirmations: {confirmations}
+          </div>
+          <div className={styles.contracts}>Contracts: {contracts}</div>
         </div>
-        <div className={styles.confirmations}>
-          Confirmations: {confirmations}
-        </div>
-        <div className={styles.contracts}>Contracts: {contracts}</div>
       </div>
     </div>
   );
