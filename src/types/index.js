222e03519a36eb78293fe26d1ffd2a3de5f874dc
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-11-22 13:25:54 +0530

    feat: 🌐 add color types

--- src/types/index.js
+++ src/types/index.js
@@ -1,8 +1,10 @@
+import { colors } from './colors';
 import { FooterTypes } from './footer';
 import { navbar } from './navbar';
 import { Theme } from './theme';
 
 export const TYPES = {
+  ...colors,
   ...FooterTypes,
   ...navbar,
   ...Theme,
