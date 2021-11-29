de53c455ec8fa188abdef2cab2b49459a2ca4cdb
shrivatsabhat <shrivatsabhatpolali@gmail.com>
2021-11-30 00:16:20 +0530

    fix: styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,6 +16,7 @@ function Navbar() {
   const navList = TYPES.navList;
   const onClickBrand = () => router.push('/');
   const [searchInput, setSearchInput] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   const DesktopNavItem = () => <div className={styles.navItem}>
     <div className={styles.links}>
@@ -39,38 +40,56 @@ function Navbar() {
   </div>
 
   const MobileNavItem = () => <div className={styles.mNavItem}>
-    <div className={styles.hamIcon}></div>
-    <Portal >
-      <section className={styles.mPortal}><div className={styles.hamMenu}></div></section>
-    </Portal>
+    <div className={styles.hamIcon} onClick={()=>setToggle(!toggle)}></div>
+    {toggle&&<section className={styles.mPortal}><div className={styles.hamMenu}>
+      <div className={styles.themeIcon}> <ThemeMode
+      onClick={() => setDarkMode((prevMode) => !prevMode)}
+      isDark={isDarkMode}
+    /></div><div className={styles.closeHam} onClick={()=>setToggle(!toggle)}></div>
+      <div className={styles.mlinks}>
+        {navList.map((navItem) => {
+          return (
+            <span
+              key={navItem.id}
+              className={
+                router.pathname === navItem.path ? styles.mactive : undefined
+              }>
+              <Link href={navItem.path}>{navItem.title}</Link>
+            </span>
+          );
+        })}
+      </div>
+    </div></section>}
   </div>
 
   return (
-    <div className={styles.container}>
-      <div className={styles.nav}>
-        <div className={styles.brand} onClick={onClickBrand}>
-          <Image
-            width={142}
-            height={32}
-            layout="fixed"
-            src={nexusLogo}
-            alt="nexus logo"></Image>
-          <div className={styles.explorer}>Explorer</div>
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.nav}>
+          <div className={styles.brand} onClick={onClickBrand}>
+            <Image
+              width={142}
+              height={32}
+              layout="fixed"
+              src={nexusLogo}
+              alt="nexus logo"></Image>
+            <div className={styles.explorer}>Explorer</div>
+          </div>
+          <DesktopNavItem />
+          <MobileNavItem />
         </div>
-        <DesktopNavItem />
-        <MobileNavItem />
+        <div className={styles.searchBar}>
+          <Search
+            long
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            onSearch={() => {
+              router.push(`/scan/${searchInput}`);
+            }}
+          />
+        </div>
       </div>
-      <div className={styles.searchBar}>
-        <Search
-          long
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          onSearch={() => {
-            router.push(`/scan/${searchInput}`);
-          }}
-        />
-      </div>
-    </div>
+    </section>
   );
 }
 
