821679d80af812e6f3e7ef3c5dee7314d22c992b
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-12-13 17:57:45 +0530

    styles: 💄 format

--- src/stories/Example/Button.jsx
+++ src/stories/Example/Button.jsx
@@ -6,14 +6,17 @@ import './button.css';
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
+      {...props}>
       {label}
     </button>
   );
--- src/stories/components/Loader.stories.js
+++ src/stories/components/Loader.stories.js
@@ -1,4 +1,4 @@
-import Loader from 'components/atoms/Loader';
+import Loader from 'components/atoms/NE_Loader';
 import TYPES from 'types';
 
 export default {
