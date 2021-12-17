ee623bb1b4baf2a3746660940f549516d1feddb1
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-12-18 01:04:25 +0530

    feat: add dev env to navbar

--- src/types/navbar.js
+++ src/types/navbar.js
@@ -1,14 +1,19 @@
 import ASSESTS from 'assets';
 
+const navList = [
+  { id: 1, title: 'Home', path: '/' },
+  { id: 2, title: 'Trustlist', path: '/trustlist' },
+  { id: 3, title: 'Richlist', path: '/richlist' },
+  { id: 4, title: 'Network', path: '/network' },
+  { id: 5, title: 'About', path: '/about' },
+];
+
 export const navbar = {
   navbar: {
-    NAVLIST: [
-      { id: 1, title: 'Home', path: '/' },
-      { id: 2, title: 'Trustlist', path: '/trustlist' },
-      { id: 3, title: 'Richlist', path: '/richlist' },
-      { id: 4, title: 'Network', path: '/network' },
-      { id: 5, title: 'About', path: '/about' },
-    ],
+    NAVLIST:
+      process.env.NODE_ENV == 'development'
+        ? [...navList, { id: 6, title: 'Test', path: '/test' }]
+        : [...navList],
     brand: ASSESTS.BRAND.PRIMARY,
   },
 };
