5ccd7411a9a58b92be4f30141fdf96467adc3316
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-01-09 12:40:38 +0530

    fixed useQuery api

--- src/components/Panel1/Panel1.js
+++ src/components/Panel1/Panel1.js
@@ -2,16 +2,50 @@ import SmallCard from '../atoms/SmallCard';
 import styles from 'components/Panel1/Panel1.module.css';
 import ChartsApex from 'components/Chart/ChartsApex';
 import { useState, useEffect } from 'react';
-import { useQuery } from 'react-query';
-import axios from 'axios';
+import Loader from 'components/atoms/NE_Loader';
+// import { useQuery } from 'react-query';
+// import axios from 'axios';
+import {GrStackOverflow} from 'react-icons/gr';
 
 function Panel1(props) {
   const { metricsRQ, infoRQ, marketRQ, miningRQ } = props;
 
   const [state, setState] = useState({});
 
+  useEffect(() => {
+    console.log('isData=', metricsRQ.isData);
+    if (metricsRQ.data) {
+      console.log('=-======', metricsRQ.data);
+      setState((prev) => ({
+        ...prev,
+        totalSupply: metricsRQ.data.data.result?.supply?.total?.toFixed(0),
+        accountRegisters: metricsRQ.data.data.result?.registers?.account,
+        inflationRate:
+          metricsRQ.data.data.result?.supply?.inflationrate?.toFixed(2),
+      }));
+    }
+
+    if (infoRQ.data) {
+      setState((prev) => ({
+        ...prev,
+        blocks: infoRQ.data.data.result?.blocks,
+      }));
+    }
+  }, [metricsRQ.data, infoRQ.data]);
+
   //  * majority of data is coming from metricsRQ , hence we use loader state of metrics for this panel
-  if (metricsRQ.isLoading) return <p>Loading...</p>;
+  if (metricsRQ.isLoading)
+    return (
+      <div
+        style={{
+          display: 'grid',
+          placeItems: 'center',
+          minHeight: '200px',
+          margin: 'auto',
+        }}>
+        <Loader type="circle" size="5rem" />
+      </div>
+    );
 
   if (metricsRQ.isError) return <p>Error...</p>;
 
@@ -28,11 +62,7 @@ function Panel1(props) {
   // }
 
   // debugger;
-  const { isData } = metricsRQ;
-  useEffect(() => {
-    // console.log(metricsRQ.data);
-  }, []);
-  
+
   // if (metricsRQ.isData) {
   //   // debugger;
   //   console.log(metricsRQ.data);
@@ -55,6 +85,7 @@ function Panel1(props) {
             text={new Intl.NumberFormat('en-US').format(state.blocks)}
             ticker="^"
             link={`/scan/${state.blocks}`}
+            icon={<GrStackOverflow/>}
           />
           <SmallCard
             label="Total Supply"
