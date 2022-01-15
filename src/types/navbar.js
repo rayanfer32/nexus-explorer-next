2f5b8bafc0b6f2e711203715dbad4d55bcc89f38
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-01-15 21:20:03 +0530

    removed network link

--- src/types/navbar.js
+++ src/types/navbar.js
@@ -4,7 +4,7 @@ const navList = [
   { id: 1, title: 'Home', path: '/' },
   { id: 2, title: 'Trustlist', path: '/trustlist' },
   { id: 3, title: 'Richlist', path: '/richlist' },
-  { id: 4, title: 'Network', path: '/network' },
+  // { id: 4, title: 'Network', path: '/network' },
   { id: 5, title: 'About', path: '/about' },
 ];
 
