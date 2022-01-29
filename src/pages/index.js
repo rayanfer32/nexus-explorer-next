a65daac17479cb7a9336ba62b894f754a6c05617
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-01-29 20:08:46 +0530

    ✨ added refresh intervals for apis

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,32 +4,56 @@ import Panel2 from 'components/Panel2/Panel2';
 import Panel3 from 'components/Panel3/Panel3';
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { refetchIntervals } from 'types/constants';
 
 export default function Home() {
-  const metricsRQ = useQuery('metrics', () => {
-    return axios.get(
-      `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/system/get/metrics`
-    );
-  });
+  const metricsRQ = useQuery(
+    'metrics',
+    () => {
+      return axios.get(
+        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/system/get/metrics`
+      );
+    },
+    {
+      refetchInterval: refetchIntervals.metrics,
+    }
+  );
 
-  const infoRQ = useQuery('info', () => {
-    return axios.get(
-      `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/system/get/info`
-    );
-  });
+  const infoRQ = useQuery(
+    'info',
+    () => {
+      return axios.get(
+        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/system/get/info`
+      );
+    },
+    {
+      refetchIntervals: refetchIntervals.info,
+    }
+  );
 
-  const marketRQ = useQuery('market', () => {
-    return axios.get(
-      `${process.env.NEXT_PUBLIC_COINGECKO_BASE_URL}/coins/nexus`
-    );
-  });
+  const marketRQ = useQuery(
+    'market',
+    () => {
+      return axios.get(
+        `${process.env.NEXT_PUBLIC_COINGECKO_BASE_URL}/coins/nexus`
+      );
+    },
+    {
+      refetchIntervals: refetchIntervals.market,
+    }
+  );
 
-  const miningRQ = useQuery('mining', () => {
-    return axios.get(
-      `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/ledger/get/info`
-    );
-  });
-
+  const miningRQ = useQuery(
+    'mining',
+    () => {
+      return axios.get(
+        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/ledger/get/info`
+      );
+    },
+    {
+      refetchIntervals: refetchIntervals.mining,
+    }
+  );
 
   return (
     <>
@@ -50,9 +74,7 @@ export default function Home() {
           miningRQ={miningRQ}
           metricsRQ={metricsRQ}
         />
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Panel3 />
-        </div>
+        <Panel3 />
       </main>
     </>
   );
