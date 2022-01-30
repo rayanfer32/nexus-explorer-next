1393efde77f3509df29a5fab70e75dc7dbb04417
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-01-30 23:41:04 +0530

    ✨ added donut chart with colured slice

--- src/components/Chart/ApexPie.jsx
+++ src/components/Chart/ApexPie.jsx
@@ -11,25 +11,11 @@ function ApexPie(props) {
   const [options, setOptions] = useState(
     props.options || {
       chart: {
-        width: 380,
-        type: 'pie',
         background: 'rgba(0, 0, 0, 0)',
       },
       theme: {
         mode: isDarkMode ? TYPES.theme.dark : TYPES.theme.light,
       },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          gradientToColors: isDarkMode
-            ? [TYPES.colors.skyBlue]
-            : [TYPES.colors.nexusBlue],
-        },
-        opacityFrom: 0.7,
-        opacityTo: 0.3,
-        stops: [0, 90, 100],
-      },
-      colors: isDarkMode ? [TYPES.colors.skyBlue] : [TYPES.colors.nexusBlue],
       labels: props.labels || [],
       responsive: [
         {
@@ -59,15 +45,6 @@ function ApexPie(props) {
   function updateChart() {
     let newOptions = { ...options };
     newOptions.theme.mode = isDarkMode ? TYPES.theme.dark : TYPES.theme.light;
-    // update colors property of the chart
-    newOptions.colors = isDarkMode
-      ? [TYPES.colors.skyBlue]
-      : [TYPES.colors.nexusBlue];
-    // update fill color of the chart
-    newOptions.fill.gradient.gradientToColors = isDarkMode
-      ? [TYPES.colors.skyBlue]
-      : [TYPES.colors.nexusBlue];
-
     setOptions(newOptions);
   }
 
@@ -77,15 +54,13 @@ function ApexPie(props) {
   }, [isDarkMode]);
 
   return (
-    <div>
       <Chart
         key={Math.random()}
         options={options}
         series={series}
-        type="pie"
+        type="donut"
         width={380}
       />
-      </div>
   );
 }
 
