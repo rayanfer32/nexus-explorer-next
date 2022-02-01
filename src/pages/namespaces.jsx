866ac92bbb1eab8773a91088699b9d82e7c5443e
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-02-01 10:21:09 +0530

    added namespaces page

---
+++ src/pages/namespaces.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import CopyText from 'components/atoms/CopyText/CopyText';
+import Loader from 'components/atoms/NE_Loader';
+import Table from 'components/Table/Table';
+import { useQuery } from 'react-query';
+
+export default function Namespaces() {
+  const { isLoading, data, error } = useQuery('namespaces', () => {
+    return axios.get(
+      `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/register/list/namespaces`
+    );
+  });
+
+  const columns = [
+    {
+      Header: 'Namespace',
+      accessor: 'namespace',
+    },
+    {
+      Header: 'Address',
+      accessor: 'address',
+      Cell: ({value}) => <CopyText value={value} />,
+    },
+    {
+      Header: 'Owner',
+      accessor: 'owner',
+      Cell: ({value}) => <CopyText value={value} />,
+    },
+    {
+      Header: 'Created',
+      accessor: 'created',
+      Cell: ({value}) => new Date(value * 1000).toDateString(),
+    },
+  ];
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          placeItems: 'center',
+          minHeight: '200px',
+          margin: 'auto',
+        }}>
+        <Loader type="circle" size="5rem" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Some Error Occured</div>;
+  }
+
+  if (data) {
+    return (
+      <div>
+        <Table columns={columns} data={data.data.result} />
+      </div>
+    );
+  }
+}
