42766e9522ddd38184b24b553768ee1c17456023
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-02-01 22:05:06 +0530

    fix: ⚡️ impreove style on disable pagination

--- src/components/Table/Pagination.jsx
+++ src/components/Table/Pagination.jsx
@@ -21,72 +21,95 @@ function Pagination({ controls }) {
     pageSize,
   } = controls;
 
+  const dataPerPage = [10, 20, 30, 40, 50];
+
+  const handleStartOfPageClick = () => {
+    gotoPage(0);
+  };
+
+  const handlePreviousPageClick = () => {
+    previousPage();
+  };
+
+  const handleNextPageClick = () => {
+    nextPage();
+  };
+
+  const handleEndOfPageClick = () => {
+    gotoPage(pageCount - 1);
+  };
+
+  const handlePageSizeChange = (e) => {
+    setPageSize(Number(e.target.value));
+  };
+
   return (
-    <div className={styles.pagination}>
-      <span className={styles.pagination__btn}>
-        <button
-          type="secondary"
-          className={styles.pagination__btn__icon}
-          onClick={() => gotoPage(0)}
-          disabled={!canPreviousPage}>
-          <BiFirstPage color="inherit" />
-        </button>
-        <button
-          className={styles.pagination__btn__icon}
-          type="secondary"
-          onClick={() => previousPage()}
-          disabled={!canPreviousPage}>
-          <BiChevronLeft color="inherit" />
-        </button>
-        <span className={styles.pagination__btn__page}>
-          Page
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>
+    <>
+      <div className={styles.pagination}>
+        <span className={styles.pagination__btn}>
+          <button
+            type="secondary"
+            className={styles.pagination__btn__icon}
+            onClick={handleStartOfPageClick}
+            disabled={!canPreviousPage}>
+            <BiFirstPage color="inherit" />
+          </button>
+          <button
+            className={styles.pagination__btn__icon}
+            type="secondary"
+            onClick={handlePreviousPageClick}
+            disabled={!canPreviousPage}>
+            <BiChevronLeft color="inherit" />
+          </button>
+          <span className={styles.pagination__btn__page}>
+            Page
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>
+          </span>
+          <button
+            className={styles.pagination__btn__icon}
+            type="secondary"
+            onClick={handleNextPageClick}
+            disabled={!canNextPage}>
+            <BiChevronRight color="inherit" />
+          </button>
+          <button
+            className={styles.pagination__btn__icon}
+            type="secondary"
+            onClick={handleEndOfPageClick}
+            disabled={!canNextPage}>
+            <BiLastPage color="inherit" />
+          </button>
         </span>
-        <button
-          className={styles.pagination__btn__icon}
-          type="secondary"
-          onClick={() => nextPage()}
-          disabled={!canNextPage}>
-          <BiChevronRight color="inherit" />
-        </button>
-        <button
-          className={styles.pagination__btn__icon}
-          type="secondary"
-          onClick={() => gotoPage(pageCount - 1)}
-          disabled={!canNextPage}>
-          <BiLastPage color="inherit" />
-        </button>
-      </span>
-      <div className={styles.pagination__goToPage}>
-        <span className={styles.pagination__goToPage__pageSelect}>
-          Go to page:
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              gotoPage(page);
-            }}
-            min={1}
-            style={{ width: '100px' }}
-          />
-        </span>
-        <select
-          className={styles.pagination__goToPage__pageDropdown}
-          value={pageSize}
-          onChange={(e) => {
-            setPageSize(Number(e.target.value));
-          }}>
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
+        <div className={styles.pagination__goToPage}>
+          <span className={styles.pagination__goToPage__pageSelect}>
+            Go to page:
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              min={1}
+              max={pageCount}
+              style={{ width: '100px' }}
+            />
+          </span>
+          <select
+            className={styles.pagination__goToPage__pageDropdown}
+            value={pageSize}
+            onChange={handlePageSizeChange}>
+            {dataPerPage.map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
