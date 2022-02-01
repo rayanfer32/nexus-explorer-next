6277034cd409c3cfc2ff436159f339f5f3d00b0c
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-02-01 21:14:44 +0530

    style: 💄 update pagination style

--- src/components/Table/Pagination.jsx
+++ src/components/Table/Pagination.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './Pagination.module.scss';
-import Button from 'components/atoms/NE_Button';
-import { BiChevronDown, BiChevronLeft, BiChevronRight, BiFirstPage, BiLastPage, BiSkipNext } from 'react-icons/bi';
+import {
+  BiChevronLeft,
+  BiChevronRight,
+  BiFirstPage,
+  BiLastPage,
+} from 'react-icons/bi';
 
 function Pagination({ controls }) {
   const {
@@ -20,60 +24,68 @@ function Pagination({ controls }) {
   return (
     <div className={styles.pagination}>
       <span className={styles.pagination__btn}>
-      <BiFirstPage
-        type="secondary"
-        onClick={() => gotoPage(0)}
-        disabled={!canPreviousPage}>
-        {'<<'}
-      </BiFirstPage>{' '}
-      <BiChevronLeft
-        type="secondary"
-        onClick={() => previousPage()}
-        disabled={!canPreviousPage}>
-        {'<'}
-      </BiChevronLeft>{' '}
-      <BiChevronRight
-        type="secondary"
-        onClick={() => nextPage()}
-        disabled={!canNextPage}>
-        {'>'}
-      </BiChevronRight>{' '}
-      <BiLastPage
-        type="secondary"
-        onClick={() => gotoPage(pageCount - 1)}
-        disabled={!canNextPage}>
-        {'>>'}
-      </BiLastPage>{' '}
-      <span>
-        Page{' '}
-        <strong>
-          {pageIndex + 1} of {pageOptions.length}
-        </strong>{' '}
-      </span>
+        <button
+          type="secondary"
+          className={styles.pagination__btn__icon}
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}>
+          <BiFirstPage color="inherit" />
+        </button>
+        <button
+          className={styles.pagination__btn__icon}
+          type="secondary"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}>
+          <BiChevronLeft color="inherit" />
+        </button>
+        <span className={styles.pagination__btn__page}>
+          Page
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
         </span>
-      <span>
-        | Go to page:{' '}
-        <input
-          type="number"
-          defaultValue={pageIndex + 1}
+        <button
+          className={styles.pagination__btn__icon}
+          type="secondary"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}>
+          <BiChevronRight color="inherit" />
+        </button>
+        <button
+          className={styles.pagination__btn__icon}
+          type="secondary"
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}>
+          <BiLastPage color="inherit" />
+        </button>
+      </span>
+      <div className={styles.pagination__goToPage}>
+        <span className={styles.pagination__goToPage__pageSelect}>
+          Go to page:
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            min={1}
+            style={{ width: '100px' }}
+          />
+        </span>
+        <select
+          className={styles.pagination__goToPage__pageDropdown}
+          value={pageSize}
           onChange={(e) => {
-            const page = e.target.value ? Number(e.target.value) - 1 : 0;
-            gotoPage(page);
-          }}
-          style={{ width: '100px' }}
-        />
-      </span>{' '}
-      <select
-        value={pageSize}
-        onChange={(e) => {
-          setPageSize(Number(e.target.value));
-        }}>
-        {[10, 20, 30, 40, 50].map((pageSize) => (
-          <option key={pageSize} value={pageSize}>
-            Show {pageSize}
-          </option>
-        ))}
-      </select>
+            setPageSize(Number(e.target.value));
+          }}>
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
