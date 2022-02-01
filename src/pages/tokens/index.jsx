d645e018de0959269a39976a9e43b9d70bc669d6
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-02-01 10:18:01 +0530

    ✨ added tokens page

---
+++ src/pages/tokens/index.jsx
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import CopyText from 'components/atoms/CopyText/CopyText';
+import Loader from 'components/atoms/NE_Loader';
+import Table from 'components/Table/Table';
+import { useQuery } from 'react-query';
+import { intlNum } from 'utils/converter';
+
+export default function Tokens() {
+  const { isLoading, data, error } = useQuery('namespaces', () => {
+    return axios.get(
+      `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/register/list/tokens?sort=maxsupply`
+    );
+  });
+
+  const columns = [
+    {
+      Header: 'Token Register Address',
+      accessor: 'token',
+      Cell: ({ value }) => <CopyText value={value} />,
+    },
+    {
+      Header: 'Token Name',
+      accessor: 'ticker',
+    },
+    {
+      Header: 'Max Supply',
+      accessor: 'maxsupply',
+      Cell: ({ value }) => intlNum(value),
+    },
+    {
+      Header: 'Current Supply',
+      accessor: 'currentsupply',
+      Cell: ({ value }) => intlNum(value),
+    },
+  ];
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          placeItems: 'center',
+          minHeight: '200px',
+          margin: 'auto',
+        }}>
+        <Loader type="circle" size="5rem" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Some Error Occured</div>;
+  }
+
+  if (data) {
+    return (
+      <div>
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        <Table columns={columns} data={data.data.result} />
+      </div>
+    );
+  }
+}
