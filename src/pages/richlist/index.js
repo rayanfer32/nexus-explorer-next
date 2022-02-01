9842cf810d53ce1364d60d9acd786a0acaf7fb77
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-02-01 10:20:23 +0530

    fix richlist

--- src/pages/richlist/index.js
+++ src/pages/richlist/index.js
@@ -5,6 +5,7 @@ import styles from './richlist.module.scss';
 import Loader from 'components/atoms/NE_Loader';
 import { intlNum, middleElipsis } from 'utils/converter';
 import ApexPie from 'components/Chart/ApexPie';
+import { MAX_SUPPLY } from 'types/constants';
 
 export default function Richlist() {
   const { isLoading, data, error } = useQuery(
@@ -14,32 +15,14 @@ export default function Richlist() {
       // * combine the result of the two queries
       // {{NEXUSTPP}}/register/list/trust?where=object.token=0 AND object.trust>10000
 
-      const accountsResponse = await axios.get(
-        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/register/list/accounts?where=object.token=0 AND object.balance>10000`,
-        {
-          params: {
-            // limit: 100,
-            // sort: 'total',
-            // order: 'desc',
-          },
-        }
+      const page0 = await axios.get(
+        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/register/list/trust,accounts?page=0&sort=total&order=desc`
+      );
+      const page1 = await axios.get(
+        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/register/list/trust,accounts?page=1&sort=total&order=desc&limit=11`
       );
 
-      const trustResponse = await axios.get(
-        `${process.env.NEXT_PUBLIC_NEXUS_BASE_URL}/register/list/trust?where=object.token=0 AND object.balance>10000`,
-        {
-          params: {
-            // limit: 100,
-            // sort: 'trust',
-            // order: 'desc',
-          },
-        }
-      );
-
-      return {
-        accounts: accountsResponse.data,
-        trust: trustResponse.data,
-      };
+      return { data: [...page0.data.result, ...page1.data.result] };
     },
     {
       // refetchOnWindowFocus: false,
@@ -50,13 +33,23 @@ export default function Richlist() {
   const columns = [
     {
       Header: 'Owner',
-      accessor: 'address',
+      accessor: 'owner',
       Cell: (props) => <div>{middleElipsis(props.value, 50)}</div>,
     },
     {
       Header: 'Balance',
       accessor: 'total',
-      Cell: (props) => intlNum(props.value) + ' NXS',
+      Cell: ({ value }) => (value ? intlNum(value) + ' NXS' : '-'),
+    },
+    {
+      Header: 'Trust',
+      accessor: 'trust',
+      Cell: ({ value }) => (value ? intlNum(value) + ' NXS' : '-'),
+    },
+    {
+      Header: 'Stake Rate',
+      accessor: 'rate',
+      Cell: ({ value }) => (value ? intlNum(value) + ' NXS' : '-'),
     },
   ];
 
@@ -81,39 +74,53 @@ export default function Richlist() {
   if (data) {
     // return <pre className={styles.page}>{JSON.stringify(data, null, 2)}</pre>;
 
-    const accountData = data.accounts.result.map((item, index) => ({
-      key: index,
-      address: item.owner,
-      total: item.total,
-    }));
+    // const accountData = data.accounts.result.map((item, index) => ({
+    //   key: index,
+    //   address: item.owner,
+    //   total: item.total,
+    // }));
 
-    const trustData = data.trust.result.map((item, index) => ({
-      key: index,
-      address: item.owner,
-      total: item.total,
-    }));
+    // const trustData = data.trust.result.map((item, index) => ({
+    //   key: index,
+    //   address: item.owner,
+    //   total: item.total,
+    // }));
 
-    const combinedData = [...accountData, ...trustData];
+    // const combinedData = [...data.page0, ...data.page1];
 
-    // filter top 100 by total from combinedData
-    const top100 = combinedData.sort((a, b) => b.total - a.total).slice(0, 100);
-    const top10 = top100.slice(0, 10);
-    const top1 = top100.slice(0, 1);
+    // const combinedData = data.page0.result.map((item, index) => ({
+    //   key: index,
+    //   owner: item.owner,
+    //   total: item.total,
+    //   trust: item.trust,
+    //   rate: item.rate,
+    // }));
 
-    const sumTop100 = top100.reduce((acc, cur) => acc + cur.total, 0);
-    const sumTop10 = top10.reduce((acc, cur) => acc + cur.total, 0);
+    // // filter top 100 by total from combinedData
+    const sortedData = data.data.sort((a, b) => b.total - a.total);
+    const top1 = sortedData.slice(0, 1);
+    const top10 = sortedData.slice(1, 11);
+    const top100 = sortedData.slice(11, 111);
+
     const sumTop1 = top1.reduce((acc, cur) => acc + cur.total, 0);
+    const sumTop10 = top10.reduce((acc, cur) => acc + cur.total, 0);
+    const sumTop100 = top100.reduce((acc, cur) => acc + cur.total, 0);
 
-    const maxSupply = 72586439.41;
-    const pieData = [sumTop1, sumTop10, sumTop100, maxSupply - sumTop100];
+    const pieData = [
+      sumTop1,
+      sumTop10,
+      sumTop100,
+      MAX_SUPPLY - (sumTop100 + sumTop10 + sumTop1),
+    ];
     const labels = ['Top 1', 'Top 10', 'Top 100', 'Others'];
+
     return (
       <div className={styles.page} style={{ marginBottom: '1rem' }}>
         <div className={styles.chartContainer}>
-        <h3>NXS Distrubution</h3>
-        <ApexPie series={pieData} labels={labels} />
+          <h3>NXS Distrubution</h3>
+          <ApexPie series={pieData} labels={labels} />
         </div>
-        <Table columns={columns} data={top100} />
+        <Table columns={columns} data={data.data} />
       </div>
     );
   }
