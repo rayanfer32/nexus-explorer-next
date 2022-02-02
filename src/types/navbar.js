b030e91178280bdf3da80ad6049dfd211d1f0d23
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-02-02 10:21:51 +0530

    💄 moved about to footer

--- src/types/navbar.js
+++ src/types/navbar.js
@@ -7,7 +7,7 @@ const navList = [
   { id: 4, title: 'Global', path: '/globalnames' },
   { id: 6, title: 'Namespaces', path: '/namespaces' },
   { id: 7, title: 'Tokens', path: '/tokens' },
-  { id: 5, title: 'About', path: '/about' },
+  // { id: 5, title: 'About', path: '/about' }, // moved to footer
 ];
 
 export const navbar = {
