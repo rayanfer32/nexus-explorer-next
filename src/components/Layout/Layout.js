5b5e6c87f233fac7f3ad4bfdfad2d1953c21496e
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-02-10 22:29:15 +0530

    feat: ✨ added scroll to top button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import styles from './Layout.module.scss';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 
 /**
  * Common layout for the nexplorer website
@@ -9,11 +10,14 @@ import Footer from '../Footer/Footer';
  */
 function Layout({ children }) {
   return (
-    <section className={styles.main}>
-      <Navbar />
-      <main className={styles.container}>{children}</main>
-      <Footer />
-    </section>
+    <>
+      <section className={styles.main}>
+        <Navbar />
+        <main className={styles.container}>{children}</main>
+        <Footer />
+      </section>
+      <ScrollToTop />
+    </>
   );
 }
 
