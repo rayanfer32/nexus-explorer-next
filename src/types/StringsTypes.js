ab751874f7d09220c354adb5953c2c651cce5676
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-03-01 23:09:38 +0530

    update metrics

--- src/types/StringsTypes.js
+++ src/types/StringsTypes.js
@@ -1,7 +1,7 @@
+import { AiOutlineCodeSandbox, AiOutlineStock } from 'react-icons/ai';
 import { BiLock, BiUser } from 'react-icons/bi';
-import { BsFilePerson } from 'react-icons/bs';
-import { GrPersonalComputer } from 'react-icons/gr';
-import { ColorsTypes } from './ColorsTypes';
+import { FaUserLock } from 'react-icons/fa';
+import { MdSupervisorAccount } from 'react-icons/md';
 
 export const StringsTypes = {
   LOADER: {
@@ -24,26 +24,38 @@ export const StringsTypes = {
 
 export const METRICS_META = {
   registers: {
-    account: { label: 'Account Registers' },
-    append: { label: 'Append Registers' },
-    crypto: { label: 'Crypto Registers' },
-    name: { label: 'Total Name Registers' },
-    name_global: { label: 'Global Name Registers' },
-    name_namespaced: { label: 'Namespaced Name Registers' },
-    namespace: { label: 'Namespace Registers' },
-    object: { label: 'Non-Standard Object Registers' },
-    object_tokenized: { label: 'Tokenized Object Registers' },
-    raw: { label: 'Raw Registers' },
-    readonly: { label: 'Read-Only Registers' },
-    token: { label: 'Token Registers' },
-    total: { label: 'Total Registers' },
+    account: { label: 'Account Registers', icon: <MdSupervisorAccount /> },
+    append: { label: 'Append Registers', icon: <AiOutlineCodeSandbox /> },
+    crypto: { label: 'Crypto Registers', icon: <AiOutlineCodeSandbox /> },
+    name: { label: 'Total Name Registers', icon: <AiOutlineCodeSandbox /> },
+    name_global: {
+      label: 'Global Name Registers',
+      icon: <AiOutlineCodeSandbox />,
+    },
+    name_namespaced: {
+      label: 'Namespaced Name Registers',
+      icon: <AiOutlineCodeSandbox />,
+    },
+    namespace: { label: 'Namespace Registers', icon: <AiOutlineCodeSandbox /> },
+    object: {
+      label: 'Non-Standard Object Registers',
+      icon: <AiOutlineCodeSandbox />,
+    },
+    object_tokenized: {
+      label: 'Tokenized Object Registers',
+      icon: <AiOutlineCodeSandbox />,
+    },
+    raw: { label: 'Raw Registers', icon: <AiOutlineCodeSandbox /> },
+    readonly: { label: 'Read-Only Registers', icon: <AiOutlineCodeSandbox /> },
+    token: { label: 'Token Registers', icon: <AiOutlineCodeSandbox /> },
+    total: { label: 'Total Registers', icon: <AiOutlineCodeSandbox /> },
   },
   reserves: {
-    ambassador: {},
-    developer: {},
-    fee: {},
-    hash: {},
-    prime: {},
+    ambassador: { ticker: 'NXS' },
+    developer: { ticker: 'NXS' },
+    fee: { ticker: 'NXS' },
+    hash: { ticker: 'NXS' },
+    prime: { ticker: 'NXS' },
   },
   sig_chains: {
     label: 'Signature Chains',
@@ -51,14 +63,19 @@ export const METRICS_META = {
     icon: <BiUser />,
   },
   supply: {
-    day: { ticker: 'NXS' },
-    hour: { ticker: 'NXS' },
-    inflationrate: { label: 'Inflation Rate', sublabel: 'Annual', ticker: '%' },
-    minute: { ticker: 'NXS' },
-    month: { ticker: 'NXS' },
+    day: { label: 'Daily', ticker: 'NXS' },
+    hour: { label: 'Hourly', ticker: 'NXS' },
+    inflationrate: {
+      label: 'Inflation Rate',
+      sublabel: 'Annual',
+      ticker: '%',
+      icon: <AiOutlineStock />,
+    },
+    minute: { label: 'Per Minute', ticker: 'NXS' },
+    month: { label: 'Monthly', ticker: 'NXS' },
     target: {},
-    total: {},
-    week: { ticker: 'NXS' },
+    total: { label: 'Total Supply' },
+    week: { label:"Weekly", ticker: 'NXS' },
   },
   trust: {
     staked_percentage: {
@@ -66,8 +83,8 @@ export const METRICS_META = {
       ticker: '%',
       icon: <BiLock />,
     },
-    stake: { label: 'Staked', ticker: 'NXS', icon: <BiLock /> },
-    total: { label: 'Staking Accounts', icon: <BsFilePerson /> },
-    trust: { label: 'Trust score' },
+    stake: { label: 'Total Staked', ticker: 'NXS', icon: <BiLock /> },
+    total: { label: 'Staking Accounts', icon: <FaUserLock /> },
+    trust: { label: 'Trust score', icon: <AiOutlineCodeSandbox /> },
   },
 };
