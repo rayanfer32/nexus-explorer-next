0107bd4df137226c186285d45218e00b651c8486
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-04-01 21:45:25 +0530

    ✨ show trust score and  stake rate instead of pending and unconfirmed

--- src/components/UserAccount/AccountInfo/AccountInfo.jsx
+++ src/components/UserAccount/AccountInfo/AccountInfo.jsx
@@ -1,9 +1,13 @@
 import styles from './AccountInfo.module.scss';
 import { NE_SmallCard as SmallCard } from 'components/atoms/NE_Card';
 import { BsBank, BsFillClockFill } from 'react-icons/bs';
-import { FaUserClock } from 'react-icons/fa';
+import { FaHandshake, FaUserClock } from 'react-icons/fa';
+import { HiChartBar } from 'react-icons/hi';
+import { intlNum } from 'utils';
 
 export function AccountInfo({ data }) {
+  const isTrust = data?.rate === null ? false : true;
+
   return (
     <>
       <section className={styles.cardsContainer}>
@@ -20,20 +24,40 @@ export function AccountInfo({ data }) {
           unit="NXS"
           icon={<BsBank />}
         />
-        <SmallCard
-          label="Pending"
-          sublabel=""
-          value={new Intl.NumberFormat().format(data?.pending || 0)}
-          unit="NXS"
-          icon={<BsFillClockFill />}
-        />
-        <SmallCard
-          label="Unconfirmed"
-          sublabel=""
-          value={data?.unconfirmed || 0}
-          unit="NXS"
-          icon={<FaUserClock />}
-        />
+        {isTrust ? (
+          <SmallCard
+            label="Stake Rate"
+            sublabel=""
+            value={data.rate || 0}
+            unit="%"
+            icon={<HiChartBar />}
+          />
+        ) : (
+          <SmallCard
+            label="Pending"
+            sublabel=""
+            value={new Intl.NumberFormat().format(data?.pending || 0)}
+            unit="NXS"
+            icon={<BsFillClockFill />}
+          />
+        )}
+        {isTrust ? (
+          <SmallCard
+            label="Trust Score"
+            sublabel=""
+            value={data?.trust || 0}
+            unit="+"
+            icon={<FaHandshake />}
+          />
+        ) : (
+          <SmallCard
+            label="Unconfirmed"
+            sublabel=""
+            value={data?.unconfirmed || 0}
+            unit="NXS"
+            icon={<FaUserClock />}
+          />
+        )}
       </section>
     </>
   );
