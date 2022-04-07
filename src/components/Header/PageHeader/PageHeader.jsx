9b2e0f8433473e118d2c77b766a0ff308fae5e41
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-04-07 21:35:30 +0530

    feat: add meta details

--- src/components/Header/PageHeader/PageHeader.jsx
+++ src/components/Header/PageHeader/PageHeader.jsx
@@ -21,26 +21,36 @@ const PageHeader = ({ page = 'HOME', title, description }) => {
       <title key="title">{_title}</title>
       <meta name="description" content={_description} />
       <link rel="canonical" href={_explorer_domain} />
+      <link rel="shortcut icon" href="/favicon2.ico" />
+      <meta
+        name="keywords"
+        content="nexus, explorer, nxs, search, blockchain, nexplorer, quantum resistance"
+      />
       <meta property="og:locale" content="en_IN" />
       <meta property="og:type" content="website" />
       <meta property="og:description" content={_description} />
       <meta property="og:url" content={_explorer_domain} />
       <meta
         property="og:image"
-        content={`${_explorer_domain}/og_meta_image.jpg` || ''}
+        content={`${_explorer_domain}/og_meta_image.jpg`}
+      />
+      <meta
+        property="og:image:url"
+        content={`${_explorer_domain}/og_meta_image.jpg`}
       />
       <meta
         property="og:image:secure_url"
-        content={`${_explorer_domain}/og_meta_image.jpg` || ''}
+        content={`${_explorer_domain}/og_meta_image.jpg`}
       />
+      <meta property="og:image:alt" content="Visit nexus.io" />
       <meta property="og:title" content={_title} />
-      <meta property="og:site_name" content="Nexus" />
+      <meta property="og:site_name" content="Nexus (NXS) Blockchain Explorer" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:description" content={_description} />
       <meta name="twitter:title" content={_title} />
       <meta
         name="twitter:image"
-        content={`${_explorer_domain}/og_meta_image.jpg` || ''}
+        content={`${_explorer_domain}/og_meta_image.jpg`}
       />
     </Head>
   );
