b2f2e3bdd4a6220b45ff0f76b310ac60a81a5263
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-05-05 23:07:32 +0530

    ✅ add some page checks

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,5 +1,65 @@
 describe('Test homepage', () => {
-  it('Renders correctly', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  function visiblityCheck() {
+    cy.get('.Panel1_container__fshdp').should('be.visible');
+    cy.get('.Panel2_panelTwoContainer__VZT4a').should('be.visible');
+    cy.get('.Panel3_container__rwlgO').should('be.visible');
+    cy.get('.Footer_footer__primary__xIc_0').should('be.visible');
+  }
+
+  it('Show Mainnet Dashboard', () => {
+    visiblityCheck();
+  });
+
+  it('Show Testnet Dashboard', () => {
+    cy.get('.SelectInput_container__K8yGb').select('Testnet');
+    visiblityCheck();
+  });
+
+  function checkAccountDivsVisibility() {
+    cy.get('.AccountInfo_cardsContainer__FUjaL').should('be.visible');
+    cy.get('.AccountDetail_details__tUWJ5').should('be.visible');
+    // 'Check if QRcode generated'
+    cy.get('.AccountDetail_qrCode__Hvv_G').should('be.visible');
+    cy.get('.TransactionDetails_page__Old5g').should('be.visible');
+  }
+
+  it('Search for username:account', () => {
+    const user_account = 'US:Interactions';
+    cy.get('.SearchBar_searchInput__zCAsW').type(`${user_account}{enter}`);
+    checkAccountDivsVisibility();
+  });
+
+  it('Search for account address', () => {
+    const account_address =
+      '8CiG14XHVz5jrgsSqNysN5dBr7N1b3bfodyAQn8LNpmr1Q2yU93';
+    cy.get('.SearchBar_searchInput__zCAsW').type(`${account_address}{enter}`);
+    checkAccountDivsVisibility();
+  });
+
+  it('Goto Metrics page', () => {
+    cy.visit('/metrics');
+    cy.contains('Signature Chains');
+    cy.contains('Total Supply');
+  });
+
+  it('Goto Blocks page', () => {
+    cy.visit('/blocks');
+    cy.get('.Table_tbody__iBkH3').should('be.visible');
+  });
+
+  it('Goto Transactions page', () => {
+    cy.visit('/transactions');
+    cy.contains('Transaction ID');
+  });
+
+  it('Goto Trustlist page', () => {
+    cy.visit('/trustlist');
+    cy.contains('Address');
+    cy.contains('Balance');
+    cy.contains('Stake');
   });
 });
