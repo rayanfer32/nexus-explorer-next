e5e3c4ec1454c2d8a0f6f484cd90c88c671b2e99
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-07-09 15:05:36 +0530

    feat: add the invoice heading

---
+++ src/components/Views/Dao/InvoiceModal/invoiceStatus.jsx
@@ -0,0 +1,3 @@
+export const InvoiceStatus = ({ status = '' }) => {
+  return <div>{status}</div>;
+};
--- src/components/Views/Dao/Invoices/index.jsx
+++ src/components/Views/Dao/Invoices/index.jsx
@@ -3,6 +3,9 @@ import { useNetwork } from 'hooks/useNetwork/useNetwork';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { columns } from './columns';
+import { BsBoxArrowLeft } from 'react-icons/bs';
+import { cls } from 'utils';
+import styles from './invoices.module.scss';
 
 function InvoicesView({ username }) {
   const { getInvoices } = useNetwork();
@@ -17,8 +20,15 @@ function InvoicesView({ username }) {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-
-  return <Table columns={columns} data={data} paginate={true} />;
+  return (
+    <>
+      <div className={cls(styles.header)}>
+        <BsBoxArrowLeft />
+        <h1>Invoices {'>>'} US:Interactions</h1>
+      </div>
+      <Table columns={columns} data={data} paginate={true} />
+    </>
+  );
 }
 
 export default InvoicesView;
