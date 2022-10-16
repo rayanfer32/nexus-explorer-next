01b34c92cf5d474a177957fb609945c4ffee068f
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-10-16 23:49:01 +0530

    feat: custom og-not -image

---
+++ src/components/Header/PageHeader/Og-Bot.jsx
@@ -0,0 +1,49 @@
+import { intlNum } from 'utils';
+
+export const OGBotImage = ({ contract, fishName, fishEmoji }) => {
+  const bg = `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/og_meta_image_bot.jpg`;
+
+  return (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'hsla(202, 97%, 52%, 1)',
+        backgroundImage: `url(${bg})`,
+        fontSize: 36,
+        color: 'white',
+      }}>
+      <h2
+        style={{
+          marginTop: 72,
+          marginLeft: 500,
+          padding: 10,
+          borderRadius: 16,
+          backgroundColor: '#012235',
+        }}>
+        {contract.OP}
+      </h2>
+      {contract.from && <h2>From - {contract.from}</h2>}
+      <span style={{ fontSize: 48 }}>{fishName}</span>
+      <span style={{ fontSize: 96 }}>{fishEmoji}</span>
+      <h1 style={{ fontSize: 72 }}>
+        {intlNum(contract.amount)} {contract.ticker}
+      </h1>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          color: '#aaa',
+          fontSize: 24,
+          marginTop: 16,
+        }}>
+        {contract.proof && <span>Proof - {contract.proof}</span>}
+        {contract.to && <span>To - {contract.to}</span>}
+      </div>
+    </div>
+  );
+};
--- src/pages/api/static/og-alert.jsx
+++ src/pages/api/static/og-alert.jsx
@@ -1,5 +1,6 @@
 import { ImageResponse } from '@vercel/og';
-import { intlNum, Log } from 'utils';
+import { OGBotImage } from 'components/Header/PageHeader/Og-Bot';
+import { Log } from 'utils';
 import { fetchTransaction } from 'utils/common/fetch';
 
 export const config = {
@@ -73,49 +74,6 @@ export default async function handler(req) {
   const [fishEmoji, fishName] = getFishnameAndEmoji(contract.amount);
 
   return new ImageResponse(
-    (
-      // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
-
-      <div
-        style={{
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          background: 'hsla(202, 97%, 52%, 1)',
-        }}>
-        <div tw="bg-slate-900 flex">
-          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-100 text-left">
-              {contract.from && (
-                <span tw="text-gray-400 text-sm">From - {contract.from}</span>
-              )}
-              {contract.proof && (
-                <span tw="text-gray-400 text-sm">Proof - {contract.proof}</span>
-              )}
-              {contract.to && (
-                <span tw="text-gray-400 text-sm">To - {contract.to}</span>
-              )}
-              <span tw="text-indigo-500">{fishName}</span>
-              <span tw="text-indigo-200 text-5xl">{fishEmoji}</span>
-              <span>
-                {intlNum(contract.amount)} {contract.ticker}
-              </span>
-            </h2>
-            <div tw="mt-8 flex md:mt-0">
-              <div tw="flex rounded-md shadow">
-                <a
-                  href="#"
-                  tw="flex items-center justify-center rounded-md border border-transparent bg-green-600 px-5 py-3 text-base font-medium text-white">
-                  {contract.OP}
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+    <OGBotImage contract={contract} fishEmoji={fishEmoji} fishName={fishName} />
   );
 }
