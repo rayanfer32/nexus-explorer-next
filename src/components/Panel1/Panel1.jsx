edc92aac832674fa32c4dba1a40138c2241e0d27
Rayanfer32 <rayanfernandes9@gmail.com>
2023-01-27 14:58:11 +0530

    refactor: Panel1

--- src/components/Panel1/Panel1.jsx
+++ src/components/Panel1/Panel1.jsx
@@ -13,18 +13,29 @@ function isInvalid(value) {
   return value == null || value == '';
 }
 
-function Panel1(props) {
-  const { metricsRQ, infoRQ, miningRQ } = props;
+function Panel1({ metricsRQ, infoRQ, miningRQ, blocks }) {
   const router = useRouter();
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    totalSupply: null,
+    sigChains: null,
+    inflationRate: null,
+    blocks: null,
+  });
 
+  // * load the state with values from the React Query response
   useEffect(() => {
     if (miningRQ.data) {
       setState((prev) => ({
         ...prev,
         totalSupply: miningRQ.data.data.result?.supply?.total?.toFixed(0),
-        sigChains: metricsRQ.data.data.result?.sigchains,
         inflationRate: miningRQ.data.data.result?.supply?.inflation?.toFixed(2),
+      }));
+    }
+
+    if (metricsRQ.data) {
+      setState((prev) => ({
+        ...prev,
+        sigChains: metricsRQ.data.data.result?.sigchains,
       }));
     }
 
@@ -34,14 +45,16 @@ function Panel1(props) {
         blocks: infoRQ.data.data.result?.blocks || 0,
       }));
     }
-  }, [metricsRQ.data, infoRQ.data]);
+  }, [metricsRQ.data, infoRQ.data, metricsRQ.data]);
 
-  if (metricsRQ.isError)
+  // * handle errors on API failure
+  if (metricsRQ.isError || miningRQ.isError || infoRQ.isError) {
     return (
       <p>
         <ErrorCard />
       </p>
     );
+  }
 
   return (
     <article className={styles.container}>
@@ -83,7 +96,7 @@ function Panel1(props) {
         />
       </section>
       <section title="chart container" className={styles.chartContainer}>
-        <ChartsApex initialData={props.blocks} />
+        <ChartsApex initialData={blocks} />
       </section>
     </article>
   );
