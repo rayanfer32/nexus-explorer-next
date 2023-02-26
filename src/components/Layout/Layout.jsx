d4823575679688460f3ae0e0f948be77b3b619f0
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2023-02-26 20:51:23 +0530

    conf: reduce animation jank

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,12 +7,12 @@ import { motion } from 'framer-motion';
 function MotionDiv({ children }) {
   return (
     <motion.div
-      initial={{ x: 300, opacity: 0 }}
+      initial={{ x: 200, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
-      exit={{ x: 300, opacity: 0 }}
+      exit={{ x: 200, opacity: 0 }}
       transition={{
-        type: 'spring',
-        stiffness: 260,
+        type: 'tween',
+        stiffness: 200,
         damping: 20,
       }}>
       {children}
