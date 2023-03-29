6291c179a98d39be42d51c3573bde356094e2a8d
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2023-03-29 17:58:37 +0530

    feat: load dao info from /public

--- src/pages/dao/ambassador/index.js
+++ src/pages/dao/ambassador/index.js
@@ -3,15 +3,27 @@ import Layout from 'components/Layout';
 import { DaoInfo } from 'components/Views/Dao';
 import React from 'react';
 import TYPES from 'types';
-import { DAO_AMBASSADORS } from 'types/DaoAccounts';
 
-function AmbassadorDAOPage() {
+function AmbassadorDAOPage({ daoObject }) {
   return (
     <Layout>
       <PageHeader title={TYPES.PAGEMETA.DAO.AMBASSADOR.TITLE} />
-      <DaoInfo title={'Ambassador DAO'} daoObject={DAO_AMBASSADORS} />
+      <DaoInfo title={'Ambassador DAO'} daoObject={daoObject} />
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const resp = await fetch(
+    `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/ambassadors.json`
+  );
+  const data = await resp.json();
+
+  return {
+    props: {
+      daoObject: data,
+    },
+  };
+};
 
 export default AmbassadorDAOPage;
